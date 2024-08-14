afda3bb20f16c6029ef8741befd970f5c4763c5f
root <root@TasnimSomo>
2024-08-14 21:22:30 +0400

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,13 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> User:
+        user self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+             if not hasattr(user, key):
+                 raise ValueError
+             setattr(user, key, value)
+
+             self._session.commit()
+             return None
